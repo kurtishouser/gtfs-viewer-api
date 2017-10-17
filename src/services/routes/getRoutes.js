5e9c6f25780eebbd43b404002eb2562023634c9a
Kurtis Houser <houserkurtis@gmail.com>
2017-10-17 09:17:11 -0700

    Add order by route_short_name to getRoutes() db query

--- src/services/routes/getRoutes.js
+++ src/services/routes/getRoutes.js
@@ -4,6 +4,7 @@ const { camelizeKeys } = require('humps');
 
 function getRoutes() {
   return knex('gtfs_routes')
+    .orderBy('route_short_name')
     .then(result => camelizeKeys(result))
     .catch(err => new Error(err));
 }
