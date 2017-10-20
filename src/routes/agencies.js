dd59c44bbde50743c64b204e72a76b6d8bae9d12
Kurtis Houser <houserkurtis@gmail.com>
2017-10-20 14:01:08 -0700

    Refactor /agencies

--- src/routes/agencies.js
+++ src/routes/agencies.js
@@ -5,34 +5,20 @@ const getAgencyById = require('../services/agencies/getAgencyById');
 
 const router = express.Router();
 
-async function getAllAgencies(req, res, next) {
-  res.locals.agencies = await getAgencies()
-    .catch(err => next(err));
-  next();
-}
+router.get('/', (req, res) => {
+  getAgencies()
+    .then((agencies) => {
+      res.json(agencies);
+    })
+    .catch(err => err);
+});
 
-async function getOneAgency(req, res, next) {
-  res.locals.agency = await getAgencyById(req.params.id)
-    .catch(err => next(err));
-  next();
-}
-
-function returnResponse(req, res) {
-  if (res.locals.agencies) {
-    return res.status(200).send(res.locals.agencies);
-    // return res.json(res.locals.agencies);
-  }
-  if (res.locals.agency) {
-    return res.status(200).send(res.locals.agency);
-  }
-  return res.sendStatus(404);
-}
-
-router.route('/')
-  .get(getAllAgencies, returnResponse);
-
-router.route('/:id')
-  .all(getOneAgency)
-  .get(returnResponse);
+router.get('/:id', (req, res) => {
+  getAgencyById(req.params.id)
+    .then((agency) => {
+      res.json(agency);
+    })
+    .catch(err => err);
+});
 
 module.exports = router;
