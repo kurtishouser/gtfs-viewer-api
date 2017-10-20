a4b011bb80fd00aa539f685214336d735b0f68e0
Kurtis Houser <houserkurtis@gmail.com>
2017-10-20 13:46:44 -0700

    Refactor /routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,17 +4,11 @@ const getRoutes = require('../services/routes/getRoutes');
 
 const router = express.Router();
 
-async function getAllRoutes(req, res, next) {
-  res.locals.routes = await getRoutes()
-    .catch(err => next(err));
-  next();
-}
-
-function returnResponse(req, res) {
-  res.status(200).send(res.locals.routes);
-}
-
-router.route('/')
-  .get(getAllRoutes, returnResponse);
+router.get('/', (req, res) => {
+  getRoutes()
+    .then((routes) => {
+      res.json(routes);
+    });
+});
 
 module.exports = router;
