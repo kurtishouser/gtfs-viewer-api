0f6cb4fb0c322a7d14bf71221e68a15a05d35654
Kurtis Houser <houserkurtis@gmail.com>
2017-11-23 10:30:17 -0800

    Improve performance of getShapes() query

--- src/services/shapes/getShapes.js
+++ src/services/shapes/getShapes.js
@@ -5,12 +5,11 @@ const { camelizeKeys } = require('humps');
 function getShapes() {
   // raw sql for now, refactor to knex(...) later
   return knex.raw('select distinct on (route_id, shapes.shape_id) \
-    shapes.shape_id, route_id, coordinates, points \
+    shapes.shape_id, route_id, coordinates \
     from ( \
       select shape_id, \
-        array_agg(array[shape_pt_lon, shape_pt_lat]) as coordinates, \
-        array_length(array_agg(array[shape_pt_lon, shape_pt_lat]), 1) as points \
-        from (select * from gtfs_shapes order by shape_id, shape_pt_sequence) as s \
+        array_agg(array[shape_pt_lon, shape_pt_lat] order by shape_id, shape_pt_sequence) as coordinates \
+        from gtfs_shapes \
       where shape_id in ( \
         select distinct shape_id \
         from gtfs_trips \
@@ -25,20 +24,3 @@ function getShapes() {
 }
 
 module.exports = getShapes;
-
-// 21 Hayes - inbound, outbound
-// where shape_id = \'147591\' or shape_id = \'147589\' \
-
-/* PREVIOUS / INTERIM VERSIONS - will delete later
-      where (service_id = \'1\' or service_id = \'1\') \
-return knex.raw('select shape_id, array_agg(array[shape_pt_lon, shape_pt_lat]) as coordinates \
-  from gtfs_shapes \
-  where shape_id in ( \
-    select distinct on (shape_id, service_id, direction_id) shape_id \
-    from gtfs_trips \
-    where (service_id = \'2\' or service_id = \'3\') \
-    order by shape_id \
-  ) \
-  group by shape_id')
-
-*/
