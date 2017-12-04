1ee69850f13ec5de9c05dd712857054393629192
Kurtis Houser <houserkurtis@gmail.com>
2017-12-04 11:15:03 -0800

    Update knexfile

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,13 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      host: process.env.RDS_HOSTNAME,
+      port: process.env.RDS_PORT,
+      database: process.env.RDS_DB_NAME,
+      user: process.env.RDS_USERNAME,
+      password: process.env.RDS_PASSWORD,
+    },
     migrations: {
       directory: `${__dirname}/db/migrations`,
     },
