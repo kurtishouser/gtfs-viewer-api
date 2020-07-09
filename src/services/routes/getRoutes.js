96936d3fe71ba742d90c8af7ea66cbca73d7a9f7
Kurtis Houser <houserkurtis@gmail.com>
2020-07-08 17:57:15 -0700

    Improve ordering criteria for getRoutes and getServices

--- src/services/routes/getRoutes.js
+++ src/services/routes/getRoutes.js
@@ -8,7 +8,7 @@ function getRoutes() {
     from ( \
     	select route_id, route_short_name, route_long_name, route_type, direction_id, json_object_agg(service_id, shape_ids) as service_ids \
     	from ( \
-    		select route_id, route_short_name, route_long_name, route_type, direction_id, service_id, array_agg(shape_id) as shape_ids \
+        select route_id, route_short_name, route_long_name, route_type, direction_id, service_id, array_agg(shape_id order by shape_id) as shape_ids \
     		from ( \
     			select distinct on (route_short_name, route_long_name, direction_id, service_id, shape_id) \
     				gtfs_trips.route_id, route_short_name, route_long_name, route_type, direction_id, service_id, shape_id \
@@ -28,36 +28,3 @@ function getRoutes() {
 }
 
 module.exports = getRoutes;
-
-
-/* PREVIOUS / INTERIM VERSIONS - will delete later
-
-return knex('gtfs_routes')
-  .orderBy('route_short_name')
-  .then(result => camelizeKeys(result))
-  .catch(err => new Error(err));
-
-return knex('gtfs_routes')
-  // .distinct(knex.raw('ON (route_short_name, route_long_name, service_id, direction_id)
-  // gtfs_trips.route_id, route_short_name, route_long_name, service_id, direction_id, shape_id'))
-  .distinct(knex.raw('ON (route_short_name, route_long_name, service_id)
-    gtfs_trips.route_id, route_short_name, route_long_name, service_id'))
-  .join('gtfs_trips', 'gtfs_routes.route_id', 'gtfs_trips.route_id')
-  //.whereIn('gtfs_routes.route_id', ['12302', '12337'])
-  .orderBy('route_short_name')
-  .orderBy('route_long_name')
-  .orderBy('service_id')
-  .orderBy('direction_id')
-  .then(result => camelizeKeys(result))
-  .catch(err => new Error(err));
-
-return knex.raw(`select route_id, route_short_name, route_long_name, route_type, array_agg(service_id) as service_ids \
-  from ( \
-    select distinct on (r.route_id, route_short_name, route_long_name, service_id) \
-    r.route_id, route_short_name, route_long_name, route_type, service_id \
-    from gtfs_routes r \
-    join gtfs_trips t on r.route_id = t.route_id \
-    order by route_short_name, route_long_name, service_id \
-  ) as s \
-  group by route_short_name, route_long_name, route_id, route_type`)
-*/
--- src/services/services/getServices.js
+++ src/services/services/getServices.js
@@ -4,6 +4,7 @@ const { camelizeKeys } = require('humps');
 
 function getServices() {
   return knex('gtfs_calendar')
+    .orderBy('start_date')
     .orderBy('service_id')
     .then(result => camelizeKeys(result));
 }
